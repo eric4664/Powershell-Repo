a7bd81a3397e555db439191b40d1133e3ad65dc1
eric4664 <eric4664@me.com>
2025-11-03 09:39:54 +0800

    Update DNS check and hash validation

--- Invoke-DownloadFile.ps1
+++ Invoke-DownloadFile.ps1
@@ -29,10 +29,10 @@ function Invoke-DownloadFile {
     $resolveIP = "10.0.0.1"
 
     # check network
-    if (!((Resolve-DnsName xensor.evaair.com).IPAddress -eq $resolveIP)) { Show-Status "Network is not in the company" -Tag FAIL; exit }
-    $Net_Hash = (Invoke-WebRequest -Uri "$Url" -Method Head).headers["x-content-sha256"] # need to add custom header for URL
-    if (!$Net_Hash) { Show-Status "Fetch $FileName hash fail." -Tag FAIL; exit }
-    if (!($Net_Hash -match "^[0-9a-zA-Z]{64}$")) {Show-Status "The format fetching form $FileName hash is not right." -Tag FAIL; exit} # check sha256 format
+    if (!((Resolve-DnsName example.com).IPAddress -eq $resolveIP)) { Show-Status "Network is not right." -Tag FAIL; exit } # check dns resolve correct
+    $Fetch_Hash = (Invoke-WebRequest -Uri "$Url" -Method Head).headers["x-content-sha256"] # check hash fetch successfully, need to add custom header for URL
+    if (!$Fetch_Hash) { Show-Status "Fetch $FileName hash fail." -Tag FAIL; exit }
+    if (!($Fetch_Hash -match "^[0-9a-zA-Z]{64}$")) {Show-Status "The $FileName hash format is not right." -Tag FAIL; exit} # check sha256 format
 
     # download 
     Invoke-WebRequest -Uri "$Url" -OutFile "$FilePath" -UseBasicParsing 
@@ -40,7 +40,7 @@ function Invoke-DownloadFile {
 
     # check hash
     $File_Hash = (Get-FileHash "$FilePath" -Algorithm SHA256).hash
-    if ($File_Hash -ne $Net_Hash) { 
+    if ($File_Hash -ne $Fetch_Hash) { 
         Show-Status "$FileName hash is not matched." -Tag FAIL
         Remove-Item -Path "$FilePath" -Force
         exit
